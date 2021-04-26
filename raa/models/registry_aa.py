21a0aa06c529c5eb2d218c2f3e6803d72f4badf1
Lisandro Gallo <4266083+lisandrogallo@users.noreply.github.com>
2021-04-26 16:27:28 -0300

    [DEL] registry_aa form view

--- raa/models/registry_aa.py
+++ raa/models/registry_aa.py
@@ -3,28 +3,33 @@ from odoo import _, api, fields, models
 
 class RegistryAA(models.Model):
 
-    _name = 'raa.registry_aa'
-    _description = 'Administrative Act Registry'
+    _name = "raa.registry_aa"
+    _description = "Administrative Act Registry"
 
-    document_id = fields.Many2one(comodel_name='tmc.document', required=True)
+    document_id = fields.Many2one(comodel_name="tmc.document", required=True)
 
     entry_date = fields.Date(default=fields.Date.context_today, required=True)
 
-    document_type_id = fields.Many2one(related='document_id.document_type_id',
-                                       readonly=True)
+    document_type_id = fields.Many2one(
+        related="document_id.document_type_id", readonly=True
+    )
 
-    dependence_id = fields.Many2one(related='document_id.dependence_id',
-                                    readonly=True,
-                                    domain=[('document_type_ids', '!=', False),
-                                            ('system_ids', 'ilike', u'RAA')],
-                                    store=True,
-                                    translate=True)
+    dependence_id = fields.Many2one(
+        related="document_id.dependence_id",
+        readonly=True,
+        domain=[
+            ("document_type_ids", "!=", False),
+            ("system_ids", "ilike", u"RAA"),
+        ],
+        store=True,
+        translate=True,
+    )
 
     number = fields.Integer(related="document_id.number", readonly=True)
 
-    period = fields.Integer(related='document_id.period',
-                            readonly=True,
-                            store=True)
+    period = fields.Integer(
+        related="document_id.period", readonly=True, store=True
+    )
 
     def name_get(self):
         result = []
@@ -37,11 +42,13 @@ class RegistryAA(models.Model):
         for raa_obj in self:
             document_obj = raa_obj.document_id
 
-            if not document_obj.date and \
-                    not document_obj.document_object and \
-                    not document_obj.main_topic_ids and \
-                    not document_obj.related_document_ids and \
-                    not document_obj.highlight_ids:
+            if (
+                not document_obj.date
+                and not document_obj.document_object
+                and not document_obj.main_topic_ids
+                and not document_obj.related_document_ids
+                and not document_obj.highlight_ids
+            ):
 
                 super(RegistryAA, raa_obj).unlink()
                 document_obj.unlink()
@@ -49,5 +56,10 @@ class RegistryAA(models.Model):
                 super(RegistryAA, raa_obj).unlink()
         return True
 
-    _sql_constraints = [('document_id_unique', 'UNIQUE(document_id)',
-                         _('Record already exists'))]
+    _sql_constraints = [
+        (
+            "document_id_unique",
+            "UNIQUE(document_id)",
+            _("Record already exists"),
+        )
+    ]
