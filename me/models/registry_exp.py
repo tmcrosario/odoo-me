5b1d4b219ce7960f5f5da3ab006569b7760cb09a
Ale Gallo <agallo0@gmail.com>
2025-06-05 10:45:58 -0300

    [ADD] Enhance registry_exp model and views; inherit from tmc.document_exp

--- me/models/registry_exp.py
+++ me/models/registry_exp.py
@@ -3,22 +3,8 @@ from odoo import fields, models
 
 class RegistryExp(models.Model):
     _name = "me.registry_exp"
+    _inherit = "tmc.document_exp"
     _description = "Expediente Registry"
-
-    name = fields.Char(string="Nombre")
-    document_id = fields.Many2one(
-        comodel_name="tmc.document",
-        required=True,
-        ondelete="cascade",
-        string="Documento",
-    )
-
-    # Campos relacionados con el documento
-    number = fields.Integer(related="document_id.number", store=True)
-    period = fields.Integer(related="document_id.period", store=True)
-    date = fields.Date(related="document_id.date", string="Fecha de inicio de trámite", store=True)
-    document_type_id = fields.Many2one(related="document_id.document_type_id", store=True)
-    dependence_id = fields.Many2one(related="document_id.dependence_id", store=True)
 
     # Campos específicos del expediente
     entry_date = fields.Date(
